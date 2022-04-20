4533339d6e9d150073d4f18c77c786d4910bad76
AKreft10 <arkadiuszkreft8@gmail.com>
2022-04-20 15:02:05 +0200

    Move mapping profiles to a separate folder Move PlaceController class to a newly created one. Create a new MappingProfile for Accommodation entity.

---
+++ DatabaseTest/MappingProfiles/AccomodationMappingProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using DatabaseTest.Entity;
+using DatabaseTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseTest.MappingProfiles
+{
+    public class AccomodationMappingProfile : Profile
+    {
+        public AccomodationMappingProfile()
+        {
+            CreateMap<Accommodation, AccommodationDto>()
+                .ForMember(p => p.Country, c => c.MapFrom(a => a.Address.Country))
+                .ForMember(p => p.City, c => c.MapFrom(a => a.Address.City))
+                .ForMember(p => p.Street, c => c.MapFrom(a => a.Address.Street))
+                .ForMember(p => p.PostalCode, c => c.MapFrom(a => a.Address.PostalCode))
+                .ForMember(p => p.Latitude, c => c.MapFrom(a => a.Address.Latitude))
+                .ForMember(p => p.Longitude, c => c.MapFrom(a => a.Address.Longitude))
+                .ForMember(p => p.PhotoGallery, c => c.MapFrom(x => x.Photos.Select(y => y.Photo).ToList()))
+                .ForMember(p => p.Reviews, c => c.MapFrom(x => x.Reviews.Select(y => y.Review).ToList()));
+
+            CreateMap<Photo, PhotoDto>();
+            CreateMap<Review, ReviewDto>();
+        }
+    }
+}
