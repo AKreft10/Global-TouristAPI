345fb328352c2099f1d208e21ebfeffa869535f3
AKreft10 <arkadiuszkreft8@gmail.com>
2022-04-21 13:31:06 +0200

    New automapper profile Mapping profile for newly created endpoint

---
+++ DatabaseTest/MappingProfiles/ReviewMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using DatabaseTest.Entity;
+using DatabaseTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseTest.MappingProfiles
+{
+    public class ReviewMappingProfile : Profile
+    {
+        public ReviewMappingProfile()
+        {
+            CreateMap<Review, ReviewDto>();
+        }
+    }
+}
