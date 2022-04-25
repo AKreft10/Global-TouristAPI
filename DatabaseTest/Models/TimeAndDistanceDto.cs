31ef2b8cc86c986e02fc14e6c1ddd3a327aa96c8
AKreft10 <arkadiuszkreft8@gmail.com>
2022-04-25 21:53:39 +0200

    Create new DTO's for newly created service Add some new types for DistanceService

---
+++ DatabaseTest/Models/TimeAndDistanceDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseTest.Models
+{
+    public class TimeAndDistanceDto
+    {
+        public double Time { get; set; }
+        public double Distance { get; set; }
+    }
+}
