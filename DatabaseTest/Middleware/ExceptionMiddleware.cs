ff7f2ee4f24cabb5fa9bb5e43fa18562c2915db7
AKreft10 <arkadiuszkreft8@gmail.com>
2022-04-27 22:24:41 +0200

    Add exception handling middleware New exception handling middlewares, to handle badrequest and notfound errors

---
+++ DatabaseTest/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using DatabaseTest.Exceptions;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseTest.Middleware
+{
+    public class ExceptionMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch(NotFoundException notFoundException)
+            {
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync(notFoundException.Message);
+            }
+            catch(BadRequestException badRequestException)
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync(badRequestException.Message);
+            }
+            catch (Exception)
+            {
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsync("Something went wrong.. ):");
+            }
+        }
+    }
+}
