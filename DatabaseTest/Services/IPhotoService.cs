2ffd7b8077130a079cf660e9a1ad5e24da85dd8a
AKreft10 <arkadiuszkreft8@gmail.com>
2022-04-28 21:11:45 +0200

    Photo service add Add new service to handle requests from PlaceService

---
+++ DatabaseTest/Services/IPhotoService.cs
@@ -0,0 +1,17 @@
+﻿using DatabaseTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseTest.Services
+{
+    public interface IPhotoService
+    {
+        Task<List<PhotoDto>>GetAllPhotos(int id);
+        Task<List<PhotoDto>> GetPhotoGallery(int id);
+        Task AddPhotosToSiteGallery(List<int> photos, int id);
+        Task SetPhotoAsAThumbnail(int photoId, int id);
+    }
+}
---
+++ DatabaseTest/Services/PhotoService.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using DatabaseTest.Entity;
+using DatabaseTest.Exceptions;
+using DatabaseTest.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseTest.Services
+{
+    public class PhotoService : IPhotoService
+    {
+        private readonly DatabaseContext _context;
+        private readonly IMapper _mapper;
+
+        public PhotoService(DatabaseContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task AddPhotosToSiteGallery(List<int> photos, int id)
+        {
+           //to do
+        }
+
+        public async Task<List<PhotoDto>> GetAllPhotos(int id)
+        {
+            var photos = await _context
+                .PlacePhotos
+                .Where(p => p.PlaceId == id)
+                .Select(ph => ph.Photo)
+                .ToListAsync();
+
+            if(photos is null)
+            {
+                throw new NotFoundException("No photos here yet.. ):");
+            }
+
+            var result = _mapper.Map<List<PhotoDto>>(photos);
+
+            return result;
+        }
+
+        public async Task<List<PhotoDto>> GetPhotoGallery(int id)
+        {
+            var photoGallery = await _context
+                .PlacePhotos
+                .Where(p => p.PlaceId == id)
+                .Select(ph => ph.Photo)
+                .Where(z => z.GalleryMember == true)
+                .ToListAsync();
+
+            if(photoGallery is null)
+            {
+                throw new NotFoundException("No photos here yet.. ):");
+            }
+
+            var result = _mapper.Map<List<PhotoDto>>(photoGallery);
+
+            return result;
+        }
+
+        public Task SetPhotoAsAThumbnail(int photoId, int id)
+        {
+            //to do
+            return Task.CompletedTask;
+        }
+    }
+}
